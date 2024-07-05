4a35f84424217c047eb1ab31634a236cffd5512d
Urmila Bisht <Urmila Bisht@Urmila>
2024-07-06 00:22:34 +0530

    I am sharing A!.java please dont get angry on me.

---
+++ src/Git/A1.java
@@ -0,0 +1,73 @@
+package Git;
+//public class A{
+//	private int a=10;
+//	private void m1() {
+//		System.out.println(a*a);
+//	}
+//}
+//
+//public class Private_class {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//       A ob1= new A();
+//       ob1.m1();
+//	}
+//
+//}
+//===============================================
+//public modifier
+//public class A1{
+//    public int a=10;
+//public void m1(){
+//  System.out.println(a*a);
+//}    
+//}
+//public class Private_class {
+//	public static void main(String[] args) {
+////		// TODO Auto-generated method stub
+//     A1 ob1= new A1();
+//    ob1.m1();
+//	}
+//
+//}
+//========================================
+////default modifier
+//class A{
+//  int a=10;
+//  void m1(){
+//System.out.println(a*a);
+//}    
+//}
+//public class Private_class {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//   A ob1= new A();
+//   ob1.m1();
+//	}
+//
+//}
+//=========================================
+////Protected modifier
+// public class A2 {
+//
+//	protected int a=10;
+//	protected void gautam() {
+//		System.out.println(a+a);
+//		System.out.println("my name is gautam");
+//		
+//	}
+//
+//}
+//  class Private_class extends A2{
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Private_class ob = new Private_class();
+//  ob.gautam();
+//	}
+//
+//}
+////
+
